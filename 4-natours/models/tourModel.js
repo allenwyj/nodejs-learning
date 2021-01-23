eec423099363df208ee62dad956e1f69e5a78a21
Yujie Wu <allenwuyujie@gmail.com>
2021-01-23 17:41:15 +1100

    modelling the Tours

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -2,9 +2,45 @@ const mongoose = require('mongoose');
 
 // Create schema
 const tourSchema = new mongoose.Schema({
-  name: { type: String, required: [true, 'Name required'], unique: true },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'Price required'] },
+  name: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  ratingsAverate: { type: Number, default: 4.5 },
+  ratingsQuantity: { type: Number, default: 0 },
+  price: { type: Number, required: [true, 'A tour must have a price'] },
+  priceDiscount: Number,
+  duration: { type: Number, required: [true, 'A tour must have a duration'] },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    trim: true,
+  },
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true, // removes whitespace from both ends of a string
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // Create model
