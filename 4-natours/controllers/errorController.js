538d19d8ed99f7b41c614898547b6f7412fad3be
Yujie Wu <allenwuyujie@gmail.com>
2021-01-27 01:34:19 +1100

    handle cast error

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,6 +10,7 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
+  console.log(err);
   // Only send error message if it is not caused by operational error.
   if (err.isOperational) {
     res.status(err.statusCode).json({
@@ -28,14 +31,23 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 // By specifying four parameters, express.js will know it is the error handler.
-module.exports = (err, req, res, next) => {
+const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
+    // Mongoose cast error: invalid ObjectId
+    if (err.name === 'CastError') err = handleCastErrorDB(err);
+
     sendErrorProd(err, res);
   }
 };
+
+module.exports = globalErrorHandler;
