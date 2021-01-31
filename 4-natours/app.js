5a6d21978adfce66a0dc7275db5fce9d3b827cba
Yujie Wu <allenwuyujie@gmail.com>
2021-02-01 05:02:40 +1100

    add mongoSanitize and xss-clean

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -37,6 +39,12 @@ app.use('/api', limiter);
 // It parses incoming requests with JSON payloads and is based on body-parser.
 // body-parser: Parse incoming request bodies
 app.use(express.json({ limit: '10kb' })); // limit within 10kb
+
+// Data sanitisation against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitisation against XSS
+app.use(xss());
 
 // Test middleware
 app.use((req, res, next) => {
