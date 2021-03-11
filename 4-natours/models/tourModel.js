21b63b9f45eb0c45e0fff8e0987d8197a6d68293
Yujie Wu <allenwuyujie@gmail.com>
2021-03-11 16:27:36 +1100

    prevent duplicated review and round up the average

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -19,6 +19,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // rounding with 1 decimal
     },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
